769072fe34500913a7f6fb39066e355b0a5af409
gemejr <gemejr4@gmail.com>
2021-06-18 04:38:36 -0700

    DDG

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,13 +2,6 @@
 #include <time.h>
 #include <stdio.h>
 
-#!/usr/bin/env perl
-# (c) 2001, Dave Jones. (the file handling bit)
-# (c) 2005, Joel Schopp <jschopp@austin.ibm.com> (the ugly bit)
-# (c) 2007,2008, Andy Whitcroft <apw@uk.ibm.com> (new conditions, test suite)
-# (c) 2008-2010 Andy Whitcroft <apw@canonical.com>
-# Licensed under the terms of the GNU GPL License version 2
-
 int main(void)
 {
 	int n;
