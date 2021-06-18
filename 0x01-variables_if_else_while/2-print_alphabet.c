123b8f8e36d77683bc40ece5afb87fb306fd4e23
gemejr <gemejr4@gmail.com>
2021-06-18 07:46:59 -0700

    FGT

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (int x = 'A'; x <= 'z'; x++)
+{
+x = tolower(x);
+putchar(x);
+}
+return (0);
+}
