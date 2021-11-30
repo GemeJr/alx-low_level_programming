301895fcff2074f637f70f7e3ce0c8ed8dc4c129
GemeJr <gemejr4@gmail.com>
2021-11-30 05:48:44 -0800

    SDFG

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,8 +10,10 @@ void print_alphabet_x10(void)
 {
 char a = 'a';
 int i;
-for (i = 0; i < 10; i++){
-for (a = 'a'; a <= 'z'; a++){
+for (i = 0; i < 10; i++)
+{
+for (a = 'a'; a <= 'z'; a++)
+{
 _putchar(a);
 }
 _putchar ('\n');
