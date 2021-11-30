86f5eb9262b733f1d3d7ce7c3997f7c41dcdf684
GemeJr <gemejr4@gmail.com>
2021-11-30 10:17:40 -0800

    SS

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <ctype.h> 
+
+int main()
+{
+char ch;    
+printf("Enter any character: ");
+scanf("%c", &ch);
+if(isupper(ch))
+{
+printf("'%c' is uppercase alphabet.", ch);
+}
+else if(islower(ch))
+{
+printf("'%c' is lowercase alphabet.", ch);
+return 1;
+}
+else
+{
+printf("'%c' is not an alphabet.", ch);
+}
+return 0;
+}
