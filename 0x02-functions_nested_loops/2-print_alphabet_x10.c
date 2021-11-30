2b23c627492339c214db74247eead2b467ed56e3
GemeJr <gemejr4@gmail.com>
2021-11-30 05:43:48 -0800

    SDFG

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_alphabet - function to print alphabet
+ *
+ *Return: no return
+ *
+ */
+void print_alphabet_x10(void)
+{
+char a = 'a';
+int i;
+for (i = 0; i < 10; i++){
+for (a = 'a'; a <= 'z'; a++){
+_putchar(a);
+}
+_putchar ('\n');
+}
+}
