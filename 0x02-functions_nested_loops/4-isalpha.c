3fcd9bceb4c7b785534cce966dc83401822eb17a
GemeJr <gemejr4@gmail.com>
2021-12-01 04:40:13 -0800

    SGF

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,12 @@
  *_isalpha -  checks for an alphabetic character
  *@c: charecter to be checked
  *
- *Return: Returns 1 if c is a letter, lowercase or uppercase Returns 0 otherwise
+ *Return: Returns 1 if c is a letter lowercase or uppercase Returns 0 otherwise
  */
 int _isalpha(int c);
 int _isalpha(int c)
 {
-if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
 return (1);
 }
